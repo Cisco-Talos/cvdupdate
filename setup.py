0e0dd3c38cc1943b2591ce43019a41d3491862f8
Micah Snyder <micasnyd@cisco.com>
2022-07-28 12:26:18 -0700

    Bump version to 1.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="cvdupdate",
-    version="1.1.0",
+    version="1.1.1",
     author="The ClamAV Team",
     author_email="clamav-bugs@external.cisco.com",
     copyright="Copyright (C) 2022 Cisco Systems, Inc. and/or its affiliates. All rights reserved.",
